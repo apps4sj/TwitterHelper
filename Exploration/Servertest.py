73c0cfdd05c2218536fb81bbdae78fc0ace184e8
apps4sj <80611533+apps4sj@users.noreply.github.com>
2021-07-31 22:33:43 -0700

    Update Servertest.py
    Updated Servertest.py to collect an image from an online device and printing it on an html file.

--- Exploration/Servertest.py
+++ Exploration/Servertest.py
@@ -1,10 +1,53 @@
 import socketserver
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    def handle(self):
-        self.data=self.request.recv(1024).strip()
-        print .__format__(self.client_address[0], self.client_address[1])
-        print(self.data)
-        self.request.sendall(self.data.lower())
+from htmlgenerate import generateHTML
+bytesToReceive = 0
+fileNameToSave = str("")
 
-server = socketserver.TCPServer(("192.168.2.139", 155), MyTCPHandler )
-server.serve_foe
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    bytesToReceive = 0
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        global bytesToReceive
+        global fileNameToSave
+        if bytesToReceive <= 0:
+            # Since there is no new number of binary bites to read, wait for receiving
+            # file info
+            fileName, fileSize = self.request.recv(1024).strip().split()
+            fileSize = int(fileSize)
+            bytesToReceive = fileSize
+            fileNameToSave = fileName.decode()
+            fileNameToSave = "/Users/KennethXu/Sites/" + fileNameToSave
+            print("Received fileName", fileName, " FileSize=", fileSize, "\n")
+        else:
+            print("Now receive binary file \n")
+            # Open a file to record received data
+            file = open(fileNameToSave, "wb")
+            while bytesToReceive > 0:
+                # Keep looping till enough bytes received.
+                # Try to buffer 256 bytes
+                rdata = self.request.recv(256)
+                # Check the length of data received
+                if len(rdata) == 0:
+                    # Connection broken. Quit looping.
+                    print("No more bytes to receive\n")
+                    break
+                else:
+                    print("received ", len(rdata), "bytes\n")
+                    # Deduct number of bytes to receive by the number of
+                    # newly received data
+                    bytesToReceive -= len(rdata)
+                    file.write(rdata)
+            print("File saved\n")
+            bytesToReceive = 0
+            fileNameToSave = ""
+            # Close file
+            file.close()
+            generateHTML()
+
+
+HOST = "192.168.2.139"
+PORT = 32421
+myServer = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+myServer.serve_forever()
